53119d7ee0e6dccbaa9b91aa91ce01b879689c97
Fernando Tsuda <ftsuda.senac@gmail.com>
2017-10-24 22:34:32 -0200

    Versão inicial do projeto Spring Boot

---
+++ oficina-festival/src/main/java/br/senac/tads/oficinafestival/OficinaFestivalApplication.java
@@ -0,0 +1,12 @@
+package br.senac.tads.oficinafestival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OficinaFestivalApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OficinaFestivalApplication.class, args);
+  }
+}
